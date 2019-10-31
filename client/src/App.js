05d33a885796add73da1b8c54e61c5f89c7393b2
Matthew Volk <volkmattj@gmail.com>
2019-10-31 10:50:15 -0500

    initial commit, working mvp

---
+++ client/src/App.js
@@ -0,0 +1,353 @@
+import React, { Component } from "react";
+import "./App.css";
+import { Container } from "reactstrap";
+import axios from "axios";
+import Logo from "./img/logo.png";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+      totalPages: [],
+      url: `/api/news?`,
+      page: "1",
+      pageSize: "100",
+      sortBy: ""
+    };
+
+    this.changePage = this.changePage.bind(this);
+    this.changePageSize = this.changePageSize.bind(this);
+    this.changeSortBy = this.changeSortBy.bind(this);
+  }
+
+  changePage = e => {
+    this.setState({ page: e.target.innerText, isLoaded: false }, () => {
+      axios
+        .get(this.state.url, {
+          params: {
+            page: this.state.page,
+            pageSize: this.state.pageSize,
+            sortBy: this.state.sortBy
+          }
+        })
+        .then(res => res.data)
+        .then(json => {
+          let range = [];
+          for (
+            var i = 1;
+            i <= Math.ceil(json.totalResults / json.pageSize);
+            i++
+          ) {
+            range.push(i);
+          }
+
+          this.setState({
+            isLoaded: true,
+            items: json,
+            totalPages: range
+          });
+        })
+        .catch(err => console.log(err));
+    });
+  };
+
+  changePageSize = e => {
+    this.setState({ pageSize: e.target.value, isLoaded: false }, () => {
+      axios
+        .get(this.state.url, {
+          params: {
+            page: this.state.page,
+            pageSize: this.state.pageSize,
+            sortBy: this.state.sortBy
+          }
+        })
+        .then(res => res.data)
+        .then(json => {
+          let range = [];
+          for (
+            var i = 1;
+            i <= Math.ceil(json.totalResults / json.pageSize);
+            i++
+          ) {
+            range.push(i);
+          }
+
+          this.setState({
+            isLoaded: true,
+            items: json,
+            totalPages: range
+          });
+        })
+        .catch(err => console.log(err));
+    });
+  };
+
+  changeSortBy = e => {
+    this.setState({ sortBy: e.target.value, isLoaded: false }, () => {
+      axios
+        .get(this.state.url, {
+          params: {
+            page: this.state.page,
+            pageSize: this.state.pageSize,
+            sortBy: this.state.sortBy
+          }
+        })
+        .then(res => res.data)
+        .then(json => {
+          let range = [];
+          for (
+            var i = 1;
+            i <= Math.ceil(json.totalResults / json.pageSize);
+            i++
+          ) {
+            range.push(i);
+          }
+
+          this.setState({
+            isLoaded: true,
+            items: json,
+            totalPages: range
+          });
+        })
+        .catch(err => console.log(err));
+    });
+  };
+
+  componentDidMount() {
+    axios
+      .get(this.state.url, {
+        params: {
+          page: this.state.page,
+          pageSize: this.state.pageSize,
+          sortBy: this.state.sortBy
+        }
+      })
+      .then(res => res.data)
+      .then(json => {
+        let range = [];
+        for (
+          var i = 1;
+          i <= Math.ceil(json.totalResults / json.pageSize);
+          i++
+        ) {
+          range.push(i);
+        }
+
+        this.setState({
+          isLoaded: true,
+          items: json,
+          totalPages: range
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    var { items, isLoaded, totalPages } = this.state;
+
+    if (!isLoaded) {
+      return (
+        <div className="App">
+          <Container fluid={true}>
+            <div className="header text-center mb-5 mt-5">
+              <img src={Logo} width="75px" alt="Ecommerce News Logo" />
+              <h1
+                style={{
+                  textTransform: "uppercase",
+                  fontWeight: "700",
+                  fontSize: "2.25em"
+                }}
+                className="mb-3"
+              >
+                E-commerce News
+              </h1>
+              {/* <div className="mb-2">
+                <h2
+                  style={{
+                    fontSize: "1rem",
+                    display: "inline",
+                    marginRight: "1em"
+                  }}
+                >
+                  <strong>Status:</strong> <kbd>Loading...</kbd>
+                </h2>
+                <h2
+                  style={{
+                    fontSize: "1rem",
+                    display: "inline",
+                    marginRight: "1em"
+                  }}
+                >
+                  <strong>Results:</strong> <kbd>Loading...</kbd>
+                </h2>
+              </div>
+              <div style={{ display: "inline", marginRight: "1em" }}>
+                <strong>Sort By:</strong>&nbsp;
+                <select onChange={e => this.changeSortBy(e)}>
+                  <option value="publishedAt">Date Published</option>
+                  <option value="relevancy">Relevency</option>
+                  <option value="popularity">Popularity</option>
+                </select>
+              </div>
+              <div style={{ display: "inline", marginRight: "1em" }}>
+                <strong>Results per Page:</strong>&nbsp;
+                <select onChange={e => this.changePageSize(e)}>
+                  <option value="100">100</option>
+                  <option value="50" disabled>
+                    50
+                  </option>
+                  <option value="25" disabled>
+                    25
+                  </option>
+                </select>
+              </div> */}
+            </div>
+            <div style={{ fontSize: "1.5em" }} className="text-center">
+              <kbd
+                style={{
+                  position: "relative",
+                  top: "50%",
+                  transform: "translateY(-50%)"
+                }}
+              >
+                Loading...
+              </kbd>
+            </div>
+          </Container>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <Container fluid={true}>
+            <div className="header text-center mb-4 mt-5">
+              <img src={Logo} width="75px" alt="Ecommerce News Logo" />
+              <h1
+                style={{
+                  textTransform: "uppercase",
+                  fontWeight: "700",
+                  fontSize: "2.25em"
+                }}
+                className="mb-3"
+              >
+                E-commerce News
+              </h1>
+              {/* <div className="mb-2">
+                <h2
+                  style={{
+                    fontSize: "1rem",
+                    display: "inline",
+                    marginRight: "1em"
+                  }}
+                >
+                  <strong>Status:</strong> <kbd>{items.status}</kbd>
+                </h2>
+                <h2
+                  style={{
+                    fontSize: "1rem",
+                    display: "inline",
+                    marginRight: "1em"
+                  }}
+                >
+                  <strong>Results:</strong> <kbd>{items.totalResults}</kbd>
+                </h2>
+              </div>
+              <div style={{ display: "inline", marginRight: "1em" }}>
+                <strong>Sort By:</strong>&nbsp;
+                <select onChange={e => this.changeSortBy(e)}>
+                  <option value="publishedAt">Date Published</option>
+                  <option value="relevancy">Relevency</option>
+                  <option value="popularity">Popularity</option>
+                </select>
+              </div>
+              <div style={{ display: "inline", marginRight: "1em" }}>
+                <strong>Results per Page:</strong>&nbsp;
+                <select onChange={e => this.changePageSize(e)}>
+                  <option value="100">100</option>
+                  <option value="50" disabled>
+                    50
+                  </option>
+                  <option value="25" disabled>
+                    25
+                  </option>
+                </select>
+              </div> */}
+            </div>
+            <div
+              className="articles"
+              style={{ maxWidth: "720px", margin: "0 auto" }}
+            >
+              {items.articles.map((article, i) => (
+                <div
+                  key={i}
+                  style={{
+                    border: "1px solid rgba(0,0,0,.1)",
+                    borderRadius: "4px",
+                    marginBottom: "1.75em",
+                    padding: "1em"
+                  }}
+                >
+                  <h5>
+                    <strong>
+                      <a
+                        href={article.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {article.title}
+                      </a>
+                    </strong>
+                  </h5>
+                  <p style={{ marginBottom: "0" }}>
+                    <strong>Source:</strong> {article.source.name} <br />
+                    <strong>Published:</strong>{" "}
+                    {new Date(article.publishedAt).toLocaleString()} <br />
+                    <strong>Author:</strong> {article.author} <br />
+                  </p>
+                </div>
+              ))}
+            </div>
+            <div
+              style={{
+                textAlign: "center",
+                marginBottom: "1em",
+                fontSize: "1.25em",
+                marginTop: "2em"
+              }}
+            >
+              <Container fluid={true}>
+                {/* Page:&nbsp;
+                {totalPages.map(page => (
+                  <div key={page} style={{ display: "inline-block" }}>
+                    <button onClick={e => this.changePage(e)}>{page}</button>
+                    &nbsp;&nbsp;
+                  </div>
+                ))} */}
+                Total Pages: {totalPages.slice(-1)[0]}
+                <p style={{ color: "red", fontSize: ".8em" }}>
+                  *Unfortunately there is an issue with{" "}
+                  <a
+                    style={{ color: "red", textDecoration: "underline" }}
+                    href="https://newsapi.org/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    https://newsapi.org/
+                  </a>{" "}
+                  that prevents us from showing any results past result #100.
+                  There are {totalPages.slice(-1)[0]} more pages of results, but
+                  we cannot allow you to browse them until News API fixes their
+                  API.
+                </p>
+              </Container>
+            </div>
+          </Container>
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const port = 4000;
+
+app.get("/api/news", (req, res) => {
+  /**
+   * @todo allow user to control pageSize from front end
+   * @todo allow user to send back which page they want to go to from front end
+   */
+
+  let { page, pageSize, sortBy } = req.query;
+  /**
+   * @note variables set below look to see if user has set value, otherwise it
+   * defaults to a variable (1, 25, publishedAt)
+   */
+  page = page || 1;
+  pageSize = pageSize || 25;
+  sortBy = sortBy || "publishedAt";
+
+  let url =
+    `https://newsapi.org/v2/everything?` +
+    `q=ecommerce&` +
+    `language=en&` +
+    `sortBy=${sortBy}&` +
+    `pageSize=${pageSize}&` +
+    `page=${page}`;
+
+  axios
+    .get(url, {
+      headers: {
+        Authorization: `Bearer ${process.env.NEWS_API_KEY}`
+      }
+    })
+    .then(data => {
+      data.data.pageSize = pageSize;
+      res.send(data.data);
+    })
+    .catch(err => res.send(err));
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
